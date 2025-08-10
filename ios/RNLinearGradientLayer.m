85d2cee2b961f468552fec9ec4caeb69a05d065d
mayank.dubey@housing.com <mayank.dubey@housing.com>
2025-08-10 20:51:44 +0530

    fix

--- ios/RNLinearGradientLayer.m
+++ ios/RNLinearGradientLayer.m
@@ -180,10 +180,10 @@
     CGSize size = self.bounds.size;
     if (!self.colors || self.colors.count == 0 || size.width == 0.0 || size.height == 0.0)
         return;
+    CGFloat inset = 0;
+    CGRect drawingRect = CGRectInset(self.bounds, inset, inset);
 
-    CGFloat *locations = nil;
-
-    locations = malloc(sizeof(CGFloat) * self.colors.count);
+    CGFloat *locations = malloc(sizeof(CGFloat) * self.colors.count);
 
     for (NSInteger i = 0; i < self.colors.count; i++)
     {
@@ -196,7 +196,7 @@
             locations[i] = (1.0 / (self.colors.count - 1)) * i;
         }
     }
-
+    
     CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
     NSMutableArray *colors = [[NSMutableArray alloc] initWithCapacity:self.colors.count];
     for (UIColor *color in self.colors) {
@@ -208,18 +208,19 @@
     free(locations);
 
     CGPoint start, end;
+    CGSize adjustedSize = drawingRect.size;
 
     if (_useAngle)
     {
         // Angle is in bearing degrees (North = 0, East = 90)
         // convert it to cartesian (N = 90, E = 0)
         float angle = (90 - _angle);
-        CGPoint relativeStartPoint = [RNLinearGradientLayer getGradientStartPointFromAngle:angle AndSize:size];
+        CGPoint relativeStartPoint = [RNLinearGradientLayer getGradientStartPointFromAngle:angle AndSize:adjustedSize];
 
         // Get true angleCenter
         CGPoint angleCenter = CGPointMake(
-            _angleCenter.x * size.width,
-            _angleCenter.y * size.height
+           _angleCenter.x * adjustedSize.width + drawingRect.origin.x,
+           _angleCenter.y * adjustedSize.height + drawingRect.origin.y
         );
         // Translate to center on angle center
         // Flip Y coordinate to convert from cartesian
@@ -235,10 +236,12 @@
     }
     else
     {
-        start = CGPointMake(self.startPoint.x * size.width, self.startPoint.y * size.height);
-        end = CGPointMake(self.endPoint.x * size.width, self.endPoint.y * size.height);
+        start = CGPointMake(self.startPoint.x * adjustedSize.width + drawingRect.origin.x,
+                            self.startPoint.y * adjustedSize.height + drawingRect.origin.y);
+        end = CGPointMake(self.endPoint.x * adjustedSize.width + drawingRect.origin.x,
+                          self.endPoint.y * adjustedSize.height + drawingRect.origin.y);
     }
-
+    CGContextClipToRect(ctx, drawingRect);
     CGContextDrawLinearGradient(ctx, gradient,
                                 start,
                                 end,
